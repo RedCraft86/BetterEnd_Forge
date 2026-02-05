e2c22c6fed062a49ac3684318bb7f2358812ec86
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-02-05 15:23:19 -0500

    Improve implementation with config toggle

--- src/main/java/org/betterx/betterend/mixin/MixinPlugin.java
+++ src/main/java/org/betterx/betterend/mixin/MixinPlugin.java
@@ -2,29 +2,16 @@ package org.betterx.betterend.mixin;
 
 import java.util.Set;
 import java.util.List;
-
-import org.slf4j.Logger;
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.loading.FMLLoader;
-
-import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import org.objectweb.asm.tree.ClassNode;
 
 public class MixinPlugin implements IMixinConfigPlugin {
-    private static final Logger LOGGER = LogUtils.getLogger();
-
-    private static boolean hasMod(String modId) {
-        return FMLLoader.getLoadingModList().getModFileById(modId) != null;
-    }
-
     @Override
     public boolean shouldApplyMixin(String targetClass, String mixinClass) {
-        if (mixinClass.contains("BlockLionfishWorldGenMixin") && hasMod("lionfishapi")) {
-            LOGGER.info("Stopping LionfishAPI's unused biome system from breaking BetterEnd's custom terrain gen.");
-            return false;
-        }
-        return true;
+        return !mixinClass.contains("BlockLionfishWorldGenMixin") ||
+                FMLLoader.getLoadingModList().getModFileById("lionfishapi") != null;
     }
 
     @Override
--- src/main/java/org/betterx/betterend/mixin/common/BlockLionfishWorldGenMixin.java
+++ src/main/java/org/betterx/betterend/mixin/common/BlockLionfishWorldGenMixin.java
@@ -1,32 +1,27 @@
 package org.betterx.betterend.mixin.common;
 
-import java.util.HashMap;
-import java.util.ArrayList;
-import com.mojang.datafixers.util.Pair;
-
-import com.github.L_Ender.lionfishapi.server.world.ModdedBiomeSlice;
+import net.minecraft.server.MinecraftServer;
+import org.betterx.betterend.world.generator.GeneratorOptions;
 import com.github.L_Ender.lionfishapi.server.world.ModdedBiomeSlicesManager;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.dimension.LevelStem;
-
+import org.slf4j.Logger;
+import com.mojang.logging.LogUtils;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = ModdedBiomeSlicesManager.class, remap = false)
+@Mixin(ModdedBiomeSlicesManager.class)
 public class BlockLionfishWorldGenMixin {
-    @Redirect(method = "onServerAboutToStart",
-            at = @At(value = "INVOKE",
-                    target = "Ljava/util/HashMap;get(Ljava/lang/Object;)Ljava/lang/Object;"
-            )
-    )
-    private static Object filterOutEndSlices(
-            HashMap<ResourceLocation, ArrayList<Pair<ResourceLocation, ModdedBiomeSlice>>> assignedSlices,
-            Object locationObj
-    ) {
+    @Unique private static final Logger BE_LOGGER = LogUtils.getLogger();
 
-        ResourceLocation location = (ResourceLocation) locationObj;
-        return location.equals(LevelStem.END.location()) ? null : assignedSlices.get(location);
+    @Inject(method = "onServerAboutToStart", at = @At("HEAD"), cancellable = true, remap = false)
+    private static void blockBiomeSlices(MinecraftServer server, CallbackInfo ci) {
+        if (GeneratorOptions.blockLionfishAPI()) {
+            BE_LOGGER.info("[BetterEnd] Stopping Lionfish API's biome system from breaking custom end terrain gen.");
+            BE_LOGGER.info("If you do not want this, disable it in BetterEnd's generator config.");
+            ci.cancel();
+        }
     }
 }
